d2e5e6edbdac67ec860d3f541c20c439dc088503
jmyungc <90362329+jmyungc@users.noreply.github.com>
2022-12-12 12:13:28 -0500

    Add files via upload

--- stocks_api.py
+++ stocks_api.py
@@ -110,9 +110,6 @@ def create_first_stock_table(cur, conn, stock_tup_list):
 
             count += 1
             stock_index += 1
-    elif (stock_count == 105):
-        print(stock_count)
-        print("First stock complete")
     else:
         return
     return
@@ -123,25 +120,19 @@ def add_second_stock(cur, conn, stock_tup_list):
     
     cur.execute("SELECT MAX(week_id) FROM Stocks WHERE stock = 'PFE'")
     start_id = cur.fetchone()
-    if start_id[0] == None:
+    if (start_id[0] == None):
         start_index = 0
     else:
         start_index = start_id[0]+1
     
-    count = 0
-
     if (stocks_in_table[0] >= 105 and stocks_in_table[0] < 210):
-        while count < 25 and start_index < len(stock_tup_list):
+        while start_index < len(stock_tup_list):
             cur.execute('INSERT OR IGNORE INTO Stocks (stock, low, high, week_id) VALUES (?,?,?,?)',(stock_tup_list[0][1], stock_tup_list[start_index][2], stock_tup_list[start_index][3], stock_tup_list[start_index][0]))
             
             conn.commit()
 
-            count += 1
             start_index += 1
             cur.execute('SELECT COUNT(*) FROM Stocks')
-    elif (stocks_in_table[0] >= 210):
-        print(stocks_in_table)
-        print("Second stock complete")
     else:
         return
     return
@@ -152,24 +143,18 @@ def add_third_stock(cur, conn, stock_tup_list):
 
     cur.execute("SELECT MAX(week_id) FROM Stocks WHERE stock = 'MRNA'")
     start_id = cur.fetchone()
-    if start_id[0] == None:
+    if (start_id[0]) == None:
         start_index = 0
     else:
         start_index = start_id[0]+1
     
-    count = 0
-
     if (stocks_in_table[0] >= 210 and stocks_in_table[0] < 315):
-        while count < 25 and start_index < len(stock_tup_list):
+        while start_index < len(stock_tup_list):
             cur.execute('INSERT OR IGNORE INTO Stocks (stock, low, high, week_id) VALUES (?,?,?,?)',(stock_tup_list[0][1], stock_tup_list[start_index][2], stock_tup_list[start_index][3], stock_tup_list[start_index][0]))
             
             conn.commit()
 
-            count += 1
             start_index += 1
-    elif (stocks_in_table[0]==315):
-        print(stocks_in_table)
-        print("Third stock complete")
     else:
         return
     return
@@ -184,6 +169,9 @@ def main(cur, conn):
     cur.execute('SELECT COUNT(*) FROM Dates')
     date_count = cur.fetchone()
 
+    if (date_count[0] == 105):
+        print("Date table completed")
+
     if (stock_count[0] < 105 and date_count[0] == 105):
         print("Adding first stock")
         jnj_stock_list = gather_stock_data('JNJ', week_list)
@@ -194,11 +182,13 @@ def main(cur, conn):
         pfe_stock_list = gather_stock_data('PFE', week_list)
         add_second_stock(cur, conn, pfe_stock_list)
 
-    elif stock_count[0] >= 210:
+    elif (stock_count[0] >= 210 and stock_count[0] < 315):
         print("Adding third stock")
         mrna_stock_list = gather_stock_data('MRNA', week_list)
         add_third_stock(cur, conn, mrna_stock_list)
 
+    elif (stock_count[0] == 315):
+        print("Stock table completed")
 
 if __name__ == "__main__":
     main()
